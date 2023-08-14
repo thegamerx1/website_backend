8ce5c3ebb3ed6bb21bf9f35bd7214617e1ba0352
thegamerx1 <34986564+thegamerx1@users.noreply.github.com>
2023-08-14 12:54:13 +0200

    Add nixy

---
+++ shell.nix
@@ -0,0 +1,27 @@
+with import <nixpkgs> { };
+
+stdenv.mkDerivation {
+
+  name = "rusty";
+  buildInputs = [ clang gcc openssl pkgconfig dbus.dev ];
+
+  # Point bindgen to where the clang library would be
+  LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+  # Make clang aware of a few headers (stdbool.h, wchar.h)
+  BINDGEN_EXTRA_CLANG_ARGS = with pkgs; ''
+    -isystem ${llvmPackages.libclang.lib}/lib/clang/${
+      lib.getVersion clang
+    }/include
+    -isystem ${llvmPackages.libclang.out}/lib/clang/${
+      lib.getVersion clang
+    }/include
+    -isystem ${glibc.dev}/include
+  '';
+
+  shellHook = ''
+    cargo ramdisk mount -t target
+  '';
+
+  # For Rust language server and rust-analyzer
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
